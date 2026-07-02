c75fde20aab1306a0ca12a7b9bd54220c3886296
Sreejith Verma <sreejith.verma@gmail.com>
2026-07-02 08:53:29 +0530

    Add loops and dynamic habit tracking

--- Day 1/main.py
+++ Day 1/main.py
@@ -2,47 +2,25 @@ print("=" * 40)
 print("    DAILY SUCCESS TRACKER")
 print("=" * 40)
 
-score = 0
-total_habits = 6
-
 name = input("What is your name? ")
 print()
 
 print("welcome", name + "!")
 print("Let's see how your day went!")
-gym = input("Did you go to the gym today? (yes/no) ")
-if gym == "yes":
-    score = score +  1
-reading = input("Did you read a book today? (yes/no) ")
-if reading == "yes":
-    score = score + 1
-content = input("Did you feel content today? (yes/no) ")
-if content == "yes":
-    score = score + 1
-meditation = input("Did you meditate today? (yes/no) ")
-if meditation == "yes":
-    score = score + 1
 
-ai_learning = input("Did you learn something about AI today? (yes/no) ")
-if ai_learning == "yes":
-    score = score + 1
-
-coding = input("Did you do any coding today? (yes/no) ")
-if coding == "yes":
-    score = score + 1
+score = 0
+habits = ["Gym", "Reading", "Content", "Meditation", "AI Learning", "Coding", "Networking"]
+total_habits = len(habits)
+for habit in habits:
+    answer = input(f"Did you do {habit} today? (yes/no) ")
+    if answer == "yes":
+        score = score + 1
 
 print()
 
 print("Today's Summary")
 print(".....................")
-print("Gym:", gym)
-print("Reading:", reading)
-print("Content:", content)
-print("Meditation:", meditation)
-print("AI Learning:", ai_learning)
-print("Coding:", coding)
-print()
-print("Today's Score:", score, "/6")
+print("Today's Score:", score, "/7")
 percentage = (score / total_habits) * 100
 print("Today's Percentage:", percentage, "%")
 if percentage == 100:
