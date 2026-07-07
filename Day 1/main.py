cab4395d28a6c7c6f72537eef6ba04aab3fc72f6
Sreejith Verma <sreejith.verma@gmail.com>
2026-07-07 15:45:43 +0530

    Refactor Success tracker using functions

--- Day 1/main.py
+++ Day 1/main.py
@@ -8,32 +8,48 @@ print()
 print("welcome", name + "!")
 print("Let's see how your day went!")
 
-score = 0
-habits = ["Gym", "Reading", "Content", "Meditation", "AI Learning", "Coding", "Networking"]
-total_habits = len(habits)
-for habit in habits:
-    answer = input(f"Did you do {habit} today? (yes/no) ")
-    if answer == "yes":
-        score = score + 1
+def collect_habits():
+       habits = ["Gym", "Reading", "Content", "Meditation", "AI Learning", "Coding", "Networking"]
+       return habits 
 
-print()
+def ask_questions(habits):
+    score = 0
+    for habit in habits:
+        answer = input(f"Did you do {habit} today? (yes/no) ")
+        if answer == "yes":
+            score = score + 1 
+    return score
+def calculate_score(score, habits):
+    percentage = (score / len(habits)) * 100
+    percentage = round(percentage, 2)
+    return percentage
 
-print("Today's Summary")
-print(".....................")
-print("Today's Score:", score, "/7")
-percentage = (score / total_habits) * 100
-print("Today's Percentage:", percentage, "%")
-if percentage == 100:
-    print("PEAK PRODUCTIVITY!")
-    print(" One step closer to dream life!")
-elif percentage >= 80:
-    print("Excellent work! Keep the momentum going!")   
-elif percentage >= 60:
-    print("Good Productivity! Consistency beats perfection!")
-elif percentage >= 50:
-    print("Average Productivity! Let's aim for more tomorrow!")
-elif percentage >= 30:
-    print("Needs improvement! Reset tomorrow!")
-else:
-    print("Today did not go as planned, The best time to reset is tomorrow!")
+def show_results(score, percentage):
+    print()
+    print("Today's Summary")
+    print(".....................")
+    print("Today's Score:", score, "/", len(habits))
+    print("Today's Percentage:", percentage, "%")
+    if percentage == 100:
+     print("PEAK PRODUCTIVITY!")
+     print(" One step closer to dream life!")
+    elif percentage >= 80:
+     print("Excellent work! Keep the momentum going!")   
+    elif percentage >= 60:
+     print("Good Productivity! Consistency beats perfection!")
+    elif percentage >= 50:
+     print("Average Productivity! Let's aim for more tomorrow!")
+    elif percentage >= 30:
+     print("Needs improvement! Reset tomorrow!")
+    else:
+     print("Today did not go as planned, The best time to reset is tomorrow!")
 
+
+
+habits = collect_habits()
+
+score = ask_questions(habits)
+
+percentage = calculate_score(score, habits)
+
+show_results(score, percentage)
